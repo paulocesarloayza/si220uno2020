4f9e4c7a0b805c2cb3661a13f9c08174b5a34778
paulocesarloayza@upsa.edu.bo <paulocesarloayza@upsa.edu.bo>
2020-02-15 10:28:50 -0400

    Mi primer modelo

---
+++ solUno/webappUno/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webappUno.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [Display(Name ="Nombre Completo")]
+        [StringLength(100,ErrorMessage ="Máximo 100 caracteres")]
+        public string Name { get; set; }
+
+        [Display(Name ="Carnet de Identidad")]
+        public string Document { get; set; }
+    }
+}
