2300e5e2d0b18620a0be4cdf0beb897d3491e6d4
paulocesarloayza@upsa.edu.bo <paulocesarloayza@upsa.edu.bo>
2020-02-15 12:02:53 -0400

    Primer MVC

---
+++ solUno/webappUno/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace webappUno.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<webappUno.Models.Person> People { get; set; }
+    }
+}
